d421fe450b709521daf92341e3f9553f59d2850e
Afshin Ghezeli <a.g.afshin@gmail.com>
2024-10-22 11:00:00 +0330

    remove unused code

--- client/src/app/_components/footer/footer.tsx
+++ client/src/app/_components/footer/footer.tsx
@@ -1,24 +1,23 @@
-import { Dropdown } from "../dropdown"
 
 export const Footer: React.FC = () => {
-    return (
-        <footer>
-            Footer
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />  
-            <br />
-        </footer>
-    )
-}
+  return (
+    <footer>
+      Footer
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </footer>
+  );
+};
